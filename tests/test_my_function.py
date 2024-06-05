864ab7fe75cc646691f361c2c352995bb38b319c
ars <arscaldgral@gmail.com>
2024-06-05 10:55:11 +0200

    completion de l'exemple

--- tests/test_my_function.py
+++ tests/test_my_function.py
@@ -1,1 +1,10 @@
 
+# tests/test_my_function.py
+
+import pytest
+from my_module.my_function import add
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
