d658b092c8f21996ec804b1d524c895a00f33ce1
ars <arscaldgral@gmail.com>
2024-06-13 16:01:30 +0200

    test 20

--- tests/test_my_function.py
+++ tests/test_my_function.py
@@ -11,8 +11,8 @@ def test_add_zero():
     assert add(0, 0) == 0, "Should be 0"
 
 def test_add_positive_and_negative():
-    assert add(1, -1) == 0, "Should be 0"
+    assert add(1, -1) == 1, "Should be 0"
 
 def test_add_large_numbers():
-    assert add(1e6, 1e6) == 2e6, "Should be 2000000"
+    assert add(1e6, 1e6) == 2e7, "Should be 2000000"
 
