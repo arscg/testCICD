6368474efd23b0d53d1f4f243e079a6159050595
ars <arscaldgral@gmail.com>
2024-06-13 15:57:32 +0200

    TEST 12

--- tests/test_my_function.py
+++ tests/test_my_function.py
@@ -1,16 +1,6 @@
-
 # tests/test_my_function.py
-
-import pytest
-from my_module.my_function import add
-
-
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+from my_module.my_function_code import add
 
 def test_add():
     assert add(1, 2) == 3
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+
