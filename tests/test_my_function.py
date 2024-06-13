a6d60f07d76f6b8bfeea6206907c45949aa1ec25
ars <arscaldgral@gmail.com>
2024-06-13 16:00:02 +0200

    tEST 13

--- tests/test_my_function.py
+++ tests/test_my_function.py
@@ -1,6 +1,18 @@
 # tests/test_my_function.py
 from my_module.my_function_code import add
 
-def test_add():
-    assert add(1, 2) == 3
+def test_add_positive_numbers():
+    assert add(1, 2) == 3, "Should be 3"
+
+def test_add_negative_numbers():
+    assert add(-1, -2) == -3, "Should be -3"
+
+def test_add_zero():
+    assert add(0, 0) == 0, "Should be 0"
+
+def test_add_positive_and_negative():
+    assert add(1, -1) == 0, "Should be 0"
+
+def test_add_large_numbers():
+    assert add(1e6, 1e6) == 2e6, "Should be 2000000"
 
